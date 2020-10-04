8fb4058c9953b404cee96c3e8617ce8ee3a1ebbc
Laercio <laerciovanderlei@gmail.com>
2020-10-04 15:56:21 -0300

    Alterando a tabela permissão
    A partir de agora não estou mais querendo que a coluna id seja mostrada na tabela permissão.

--- permissao/lista_permissao.php
+++ permissao/lista_permissao.php
@@ -1,20 +1,20 @@
 <div class="container">
   <h2>Permissões</h2>
   <a class="btn btn-info" href="permissao.php?acao=novo">Novo</a>
-  
+
   <?php if (count($registros)==0): ?>
     <p>Nenhuma permissão encontrado.</p>
   <?php else: ?>
     <table class="table table-hover table-stripped">
       <thead>
-           <th>#</th>
+           <!-- <th>#</th> -->
 	         <th>Nome</th>
            <th>Ações</th>
       </thead>
       <tbody>
         <?php foreach ($registros as $linha): ?>
           <tr>
-      <td><?php echo $linha['id']; ?></td>
+      <!-- <td><?php echo $linha['id']; ?></td> -->
 	    <td><?php echo $linha['nome']; ?></td>
 	          <td>
                 <a class="btn btn-primary btn-sm" href="permissao.php?acao=buscar&id=<?php echo $linha['id']; ?>">Editar</a>
