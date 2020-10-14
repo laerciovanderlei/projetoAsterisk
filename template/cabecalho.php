9bc326c79b1a31b85a653561c66ec6b8c5ff63ee
Laercio Vanderlei Grzelak <laerciovanderlei@gmail.com>
2020-10-14 20:49:25 -0300

    cabeçalho
    Removi um botão que continha acesso aos menus antigos

--- template/cabecalho.php
+++ template/cabecalho.php
@@ -96,9 +96,9 @@
 
 
 
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+          <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
-          </button>
+          </button> -->
         </div>
       </div>
     </header>
